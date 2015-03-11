a7f221506a3e62b58169b837a324c4bf958f1e38
1107AND <1107AND@6A-08>
2015-03-11 21:21:20 +0900

    インテントによるTwitterとfacebook連携を実装。ダミーレイアウトの変更。

--- src/jp/egaonohon/camerapet/MainActivity.java
+++ src/jp/egaonohon/camerapet/MainActivity.java
@@ -2,21 +2,33 @@ package jp.egaonohon.camerapet;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
+/**
+ *
+ * カメラペットのMainActivityのクラスです。 onClickFacebookBtnとonClickTwitterBtnボタンは要差し替え。
+ *
+ * @author 1107AND
+ *
+ */
 public class MainActivity extends Activity {
 
-	Button goCam;
+	private final int FACEBOOK_ID = 0;
+	private final int TWITTER_ID = 1;
+	private final String[] sharePackages = { "com.facebook.katana",
+			"com.twitter.android" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		findViewById();
 	}
 
 	@Override
@@ -38,10 +50,6 @@ public class MainActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	public void findViewById() {
-		goCam = (Button) findViewById(R.id.goCamBtn);// 更新依頼ボタンの参照を引っ張ってくる
-	}
-
 	public void onClickGoCamBtn(View v) {
 		/**
 		 * 画面移動要求を格納したインテントを作成する。 第一引数に自身(this)を設定 第二引数に移動先のクラス名を指定
@@ -51,6 +59,73 @@ public class MainActivity extends Activity {
 		/**
 		 * Activity.startActivity()の第一引数にインテントを指定することで画面移動が行われる。
 		 */
+		startActivity(intent);
+	}
+
+	public void onClickTwitterBtn(View v) {
+		if (isShareAppInstall(TWITTER_ID)) {
+			Intent intent = new Intent();
+			intent.setAction(Intent.ACTION_SEND);
+			intent.setPackage(sharePackages[TWITTER_ID]);
+			intent.setType("image/png");
+			/**
+			 * 　URLはアプリのダウンロードページに差し替えること!
+			 */
+			intent.putExtra(Intent.EXTRA_TEXT,
+					"Androidアプリ「カメラペット」で飼っているペット。今こんな感じです。"
+							+ "https://play.google.com/store/apps/");
+			startActivity(intent);
+		} else {
+			shareAppDl(TWITTER_ID);
+		}
+	}
+
+	public void onClickFacebookBtn(View v) {
+		if (isShareAppInstall(FACEBOOK_ID)) {
+			Intent intent = new Intent();
+			intent.setAction(Intent.ACTION_SEND);
+			intent.setPackage(sharePackages[FACEBOOK_ID]);
+			intent.setType("text/plain");
+			/**
+			 * 　URLはアプリのダウンロードページに差し替えること!
+			 */
+			intent.putExtra(Intent.EXTRA_TEXT,
+					"https://play.google.com/store/apps/");
+			startActivity(intent);
+		} else {
+			shareAppDl(FACEBOOK_ID);
+		}
+	}
+
+	/**
+	 * アプリがインストールされているかチェックするメソッド。
+	 *
+	 * @param shareId
+	 * @return
+	 */
+	private Boolean isShareAppInstall(int shareId) {
+		try {
+			PackageManager pm = getPackageManager();
+			pm.getApplicationInfo(sharePackages[shareId],
+					PackageManager.GET_META_DATA);
+			return true;
+		} catch (NameNotFoundException e) {
+			/**
+			 * エラー発生時のトースト。
+			 */
+			Toast.makeText(this, "何らかのエラーが発生しました", Toast.LENGTH_SHORT).show();
+			return false;
+		}
+	}
+
+	/**
+	 * アプリが無かったのでGooglePalyに飛ばすメソッド。
+	 *
+	 * @param shareId
+	 */
+	private void shareAppDl(int shareId) {
+		Uri uri = Uri.parse("market://details?id=" + sharePackages[shareId]);
+		Intent intent = new Intent(Intent.ACTION_VIEW, uri);
 		startActivity(intent);
 	}
 }
